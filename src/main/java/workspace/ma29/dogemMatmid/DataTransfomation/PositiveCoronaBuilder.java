8e1c86f651ac69a016dd8ea82ae75ce8d3d86006
Guy <95033937+roncheers@users.noreply.github.com>
2022-01-17 19:07:54 +0200

    positiveCB needsTesting

--- src/main/java/workspace/ma29/dogemMatmid/DataTransfomation/PositiveCoronaBuilder.java
+++ src/main/java/workspace/ma29/dogemMatmid/DataTransfomation/PositiveCoronaBuilder.java
@@ -14,18 +14,16 @@ import java.util.stream.Collectors;
 public class PositiveCoronaBuilder {
     public ArrayList<PositivePerson> build(ArrayList<LabTest> labTests, ArrayList<Report> reports) {
         ArrayList<PositivePerson> positivePeople = new ArrayList<PositivePerson>();
-        System.out.println(labTests.size());
-        System.out.println(reports.size());
         Map<String, LabTest> labTestsMap = labTests.stream().collect(Collectors.toMap(LabTest::getStickerNumber, Function.identity()));
         Map<String, Report> reportsMap = reports.stream().collect(Collectors.toMap(Report::getBarcode, Function.identity()));
-//        for (String key : reportsMap.keySet()) {
-//            if (labTestsMap.containsKey(key) && labTestsMap.get(key).getResultTestCorona().equals("1")) {
-//                LabTest l = labTestsMap.get(key);
-//                Report r = reportsMap.get(key);
-//                positivePeople.add(new PositivePerson(r.getIdNum(),r.getIdType(),r.getFirstName(),r.getLastName(),r.getCity(),r.getStreet(),r.getBuildingNumber(),r.getBarcode(),
-//                        l.getBirthDate(),l.getLabCode(),l.getResultDate(),r.getTakeDate(),l.getStickerNumber(), l.getResultTestCorona(),l.getVariant(),l.getTestType()));
-//            }
-//        }
+        for (String key : reportsMap.keySet()) {
+            if (labTestsMap.containsKey(key) && labTestsMap.get(key).getResultTestCorona().equals("1")) {
+                LabTest l = labTestsMap.get(key);
+                Report r = reportsMap.get(key);
+                positivePeople.add(new PositivePerson(r.getIdNum(),r.getIdType(),r.getFirstName(),r.getLastName(),r.getCity(),r.getStreet(),r.getBuildingNumber(),r.getBarcode(),
+                        l.getBirthDate(),l.getLabCode(),l.getResultDate(),r.getTakeDate(),l.getStickerNumber(), l.getResultTestCorona(),l.getVariant(),l.getTestType()));
+            }
+        }
         return positivePeople;
     }
 }
